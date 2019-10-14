20784c6c39bfa12f1e1fd443aa11b21adc286e48
ironman.w <ironman@gmail.com>
2019-10-14 14:53:24 +0800

    add smart pull

--- lib/src/widget/smart_pull_refresh.dart
+++ lib/src/widget/smart_pull_refresh.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart';
+import 'package:titan/src/global.dart';
 
 class SmartPullRefresh extends StatefulWidget {
   final Widget child;
@@ -29,12 +30,20 @@ class _SmartPullRefreshState extends State<SmartPullRefresh> {
         header: WaterDropHeader(),
         onRefresh: () async {
           await Future.delayed(Duration(milliseconds: 1000));
-          widget.onRefresh();
+          try {
+            widget.onRefresh();
+          } catch (_) {
+            logger.e(_);
+          }
           _refreshController.refreshCompleted();
         },
         onLoading: () async {
           await Future.delayed(Duration(milliseconds: 1000));
-          widget.onLoading();
+          try {
+            widget.onLoading();
+          } catch (_) {
+            logger.e(_);
+          }
           _refreshController.loadComplete();
         },
         child: widget.child);
