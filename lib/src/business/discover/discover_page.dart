9ba5dd3475fe8badfc6544aadb2e21ef9f40eac0
spider.m <spider@gmail.com>
2019-10-17 15:44:33 +0800

    update some ui

--- lib/src/business/discover/discover_page.dart
+++ lib/src/business/discover/discover_page.dart
@@ -49,7 +49,7 @@ class DiscoverPageState extends State<DiscoverPageWidget> {
               child: Column(
                 children: <Widget>[
                   SizedBox(
-                    height: 260,
+                    height: 290,
                     child: Stack(
                       children: <Widget>[
                         SizedBox(
@@ -181,26 +181,34 @@ class DiscoverPageState extends State<DiscoverPageWidget> {
                                                   children: <Widget>[
                                                     Image.asset(
                                                       'res/drawable/ic_dmap_mbassy.png',
-                                                      width: 32,
-                                                      height: 32,
+                                                      width: 28,
+                                                      height: 28,
                                                     ),
-                                                    Padding(
-                                                      padding: const EdgeInsets.only(left: 8.0),
-                                                      child: Column(
-                                                        crossAxisAlignment: CrossAxisAlignment.start,
-                                                        children: <Widget>[
-                                                          Text(
-                                                            '大使馆指南',
-                                                            style: TextStyle(fontWeight: FontWeight.w600),
-                                                          ),
-                                                          Padding(
-                                                            padding: const EdgeInsets.only(top: 8.0),
-                                                            child: Text(
-                                                              '全球大使馆地图',
-                                                              style: TextStyle(color: Colors.grey, fontSize: 13),
+                                                    Expanded(
+                                                      child: Padding(
+                                                        padding: const EdgeInsets.only(left: 8.0),
+                                                        child: Column(
+                                                          crossAxisAlignment: CrossAxisAlignment.start,
+                                                          children: <Widget>[
+                                                            Text(
+                                                              '大使馆指南',
+                                                              style: TextStyle(fontWeight: FontWeight.w600),
                                                             ),
-                                                          ),
-                                                        ],
+                                                            Padding(
+                                                              padding: const EdgeInsets.only(top: 8.0),
+                                                              child: Row(
+                                                                children: <Widget>[
+                                                                  Expanded(
+                                                                    child: Text(
+                                                                      '全球大使馆',
+                                                                      style: TextStyle(color: Colors.grey, fontSize: 13),
+                                                                    ),
+                                                                  ),
+                                                                ],
+                                                              ),
+                                                            ),
+                                                          ],
+                                                        ),
                                                       ),
                                                     )
                                                   ],
@@ -227,8 +235,8 @@ class DiscoverPageState extends State<DiscoverPageWidget> {
                                                   children: <Widget>[
                                                     Image.asset(
                                                       'res/drawable/ic_dmap_bar.png',
-                                                      width: 32,
-                                                      height: 32,
+                                                      width: 28,
+                                                      height: 28,
                                                     ),
                                                     Padding(
                                                       padding: const EdgeInsets.only(left: 8.0),
