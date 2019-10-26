e7494cd4613c33354920ebbdb2dd9d9f7b7fc459
ironman.w <ironman@gmail.com>
2019-10-26 11:48:22 +0800

    modify discover image

--- lib/src/business/discover/discover_page.dart
+++ lib/src/business/discover/discover_page.dart
@@ -18,10 +18,7 @@ class DiscoverPageWidget extends StatefulWidget {
 }
 
 class DiscoverPageState extends State<DiscoverPageWidget> {
-  List<FocusImage> focusImages = [
-    FocusImage(
-        "https:\/\/news.hyn.space\/wp-content\/uploads\/2019\/10\/WechatIMG16-768x443.jpeg", "https://www.hyn.space")
-  ];
+  List<FocusImage> focusImages = [FocusImage('res/drawable/discover_first_image.jpeg', "https://www.hyn.space")];
 
   @override
   void initState() {
@@ -60,6 +57,8 @@ class DiscoverPageState extends State<DiscoverPageWidget> {
                         SizedBox(
                           height: 220,
                           child: Carousel(
+                            borderRadius: true,
+                            radius: Radius.circular(0),
                             onImageTap: (int index) {
                               var focusImage = focusImages[index];
 
@@ -78,7 +77,13 @@ class DiscoverPageState extends State<DiscoverPageWidget> {
                             },
                             dotVerticalPadding: 16,
                             dotBgColor: Colors.transparent,
-                            images: focusImages.map((focusImage) => NetworkImage(focusImage.cover)).toList(),
+                            images: focusImages.map((focusImage) {
+                              return FadeInImage.assetNetwork(
+                                placeholder: 'res/drawable/img_placeholder.jpg',
+                                image: focusImage.cover,
+                                fit: BoxFit.cover,
+                              );
+                            }).toList(),
                           ),
                         ),
                         Align(
@@ -222,7 +227,8 @@ class DiscoverPageState extends State<DiscoverPageWidget> {
                                                                   Expanded(
                                                                     child: Text(
                                                                       '全球大使馆',
-                                                                      style: TextStyle(color: Colors.grey, fontSize: 13),
+                                                                      style:
+                                                                          TextStyle(color: Colors.grey, fontSize: 13),
                                                                     ),
                                                                   ),
                                                                 ],
