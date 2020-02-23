156f72a8e4c98853f0d9f588bc484a5f8a98593d
Javion <javion@163.com>
2020-02-23 10:36:40 +0800

    add android xiaomi huawei push

--- android/umenglib/src/main/java/org/hyn/titan/umenglib/push/UMengPushImpl.kt
+++ android/umenglib/src/main/java/org/hyn/titan/umenglib/push/UMengPushImpl.kt
@@ -1,12 +1,16 @@
 package org.hyn.titan.umenglib.push
 
+import android.app.Application
 import android.content.Context
 import android.util.Log
 import android.widget.Toast
+import com.taobao.accs.utl.ALog
 import com.umeng.commonsdk.UMConfigure
 import org.hyn.titan.umenglib.interfaces.IUMengPush
 import com.umeng.message.IUmengRegisterCallback
 import com.umeng.message.PushAgent
+import org.android.agoo.huawei.HuaWeiRegister
+import org.android.agoo.xiaomi.MiPushRegistar
 import org.hyn.titan.umenglib.interfaces.OnPushChangeListener
 
 
@@ -17,7 +21,8 @@ class UMengPushImpl : IUMengPush{
 
     override fun initUMeng(context: Context) {
         Log.i(LOG_TAG, "initUMeng")
-        UMConfigure.init(context, "5e4b54e8570df363b800014c", "Umeng", UMConfigure.DEVICE_TYPE_PHONE, "baa106475d99b7351420ef740eb2a24f")
+        UMConfigure.setLogEnabled(true)
+        UMConfigure.init(context, UMengConstants.UMENG_APPKEY, "Umeng", UMConfigure.DEVICE_TYPE_PHONE, UMengConstants.UMENG_MESSAGE_SECRET)
 
         //获取消息推送代理示例
         val mPushAgent = PushAgent.getInstance(context)
@@ -27,13 +32,16 @@ class UMengPushImpl : IUMengPush{
                 //注册成功会返回deviceToken deviceToken是推送消息的唯一标志
                 Log.i(LOG_TAG, "register success：deviceToken：-------->  $deviceToken")
                 umengToken = deviceToken
-//                onPushChangeListener.onTokenSuccess(deviceToken)
             }
 
             override fun onFailure(s: String, s1: String) {
                 Log.e(LOG_TAG, "register fail：-------->  s:$s,s1:$s1")
             }
         })
+
+        MiPushRegistar.register(context, UMengConstants.UMENG_XIAOMI_APPID, UMengConstants.UMENG_XIAOMI_SECRET)
+
+        HuaWeiRegister.register(context as Application)
     }
 
     override fun getUMengToken() : String {
