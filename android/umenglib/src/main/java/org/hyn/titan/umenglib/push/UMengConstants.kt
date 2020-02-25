1b6ab68fc48e147ffa193ad95121b3ac8e61e65b
Javion <javion@163.com>
2020-02-25 16:13:51 +0800

    add android meizu push

--- android/umenglib/src/main/java/org/hyn/titan/umenglib/push/UMengConstants.kt
+++ android/umenglib/src/main/java/org/hyn/titan/umenglib/push/UMengConstants.kt
@@ -10,4 +10,8 @@ object UMengConstants{
     /**华为需要修改manifest的meta-data**/
     const val UMENG_HUAWEI_APPID = "101755081"
     const val UMENG_HUAWEI_SECRET = "ac3f0fd843ee6210cfbe79eb4faf14408fe35e119318df53306b2fc7f91e01c8"
+
+    const val UMENG_MEIZU_APPID = "128062"
+    const val UMENG_MEIZU_APPKEY = "a446881639344dfc9c2652efa0d8eff2"
+    const val UMENG_MEIZU_SECRET = "5399157a934547e08cea74809a8b1600"
 }
