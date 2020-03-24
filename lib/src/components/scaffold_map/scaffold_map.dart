ea93b390848a30eeb6a11b1b90a0979211964947
Javion <hhdbusiness@icloud.com>
2020-03-24 16:44:54 +0800

    fix home panel reset default state

--- lib/src/components/scaffold_map/scaffold_map.dart
+++ lib/src/components/scaffold_map/scaffold_map.dart
@@ -392,6 +392,10 @@ class _ScaffoldMapState extends State<ScaffoldMap> {
         WidgetsBinding.instance.addPostFrameCallback((_) {
           myWidget.DraggableScrollableActuator.reset(Keys.mapDraggablePanelKey.currentContext);
         });
+      }else{
+        WidgetsBinding.instance.addPostFrameCallback((_) {
+          myWidget.DraggableScrollableActuator.setHide(Keys.mapDraggablePanelKey.currentContext);
+        });
       }
 
       return Stack(
