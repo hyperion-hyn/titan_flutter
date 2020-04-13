65ffe3e437738ebd79b8a58c5cf295e1110bdb14
naruto.j <15115790290@hot.com>
2020-04-13 20:43:40 +0800

    Update contract_const.dart

--- lib/src/plugins/wallet/contract_const.dart
+++ lib/src/plugins/wallet/contract_const.dart
@@ -2,25 +2,27 @@
 
 /*
 
-Wallet Address: 0x5767b8B0CD6d1Fb24Ce44d76B24E58008E77a19d
-Private key: 913e63e87a5f491dca320d4cc8a9d5dfada433cfd031f16ebc7b2be33c9f50a6
-Staking contract: 0x301A8856256a8B028C2Da99d2e2379F24E9087B1
-ERC20: 0xe51bae7F7caf6Cb12d5A5e75E0947eC4fAf44688
-IP: 10.10.1.115
-port: 7545,
+Wallet Address: 0x7C52f69ce94a2F73BC4fd2fA9265D0ceab79c4e3
+Private key: 5e960e6e5ee9f0eb84083aeb2c82e88d806e61884aefa62d85d2ce7bb3c757b0
+Staking contract: 0xEf429f2F9C4AB4a0723dEf4C76A8b5499dEB7205
+ERC20: 0x884845609ea5DC317FA74Ccc5bdEc161Ce58D357
+IP: 219.137.207.27
+port: 37545,
 network_id: 5777
 */
 
 
+
+
 class ContractTestConfig {
 
-  static const String ERC20 = "0xe51bae7F7caf6Cb12d5A5e75E0947eC4fAf44688";
-  static const String stakingContract = "0x301A8856256a8B028C2Da99d2e2379F24E9087B1";
-  static const String privateKey = "913e63e87a5f491dca320d4cc8a9d5dfada433cfd031f16ebc7b2be33c9f50a6";
+  static const String ERC20 = "0x884845609ea5DC317FA74Ccc5bdEc161Ce58D357";
+  static const String stakingContract = "0xEf429f2F9C4AB4a0723dEf4C76A8b5499dEB7205";
+  static const String privateKey = "5e960e6e5ee9f0eb84083aeb2c82e88d806e61884aefa62d85d2ce7bb3c757b0";
 
-//  static const String walletLocalDomain = "http://116.23.19.213:37545";
-  static const String walletLocalDomain = "http://10.10.1.115:7545";
+  static const String walletLocalDomain = "http://219.137.207.27:37545";
+//  static const String walletLocalDomain = "http://10.10.1.115:7545";
 
-//  static const String apiLocalDomain = "http://116.23.19.213:35000/";
-  static const String apiLocalDomain = "http://10.10.1.115:5000/";
+  static const String apiLocalDomain = "http://219.137.207.27:35000/";
+//  static const String apiLocalDomain = "http://10.10.1.115:5000/";
 }
