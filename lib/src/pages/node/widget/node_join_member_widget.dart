e1759d9f02fbb0d38a0806034e79d82d861a685f
Javion <hhdbusiness@icloud.com>
2020-04-20 11:51:32 +0800

    fix no more data

--- lib/src/pages/node/widget/node_join_member_widget.dart
+++ lib/src/pages/node/widget/node_join_member_widget.dart
@@ -64,7 +64,7 @@ class _NodeJoinMemberState extends State<NodeJoinMemberWidget> {
   void getJoinMemberData() async {
     _currentPage = 0;
     List<ContractDelegatorItem> tempMemberList =
-        await _nodeApi.getContractDelegator(int.parse(widget.contractId), page: _currentPage);
+    await _nodeApi.getContractDelegator(int.parse(widget.contractId), page: _currentPage);
 
     // print("[widget] --> build, length:${tempMemberList.length}");
 
@@ -77,7 +77,7 @@ class _NodeJoinMemberState extends State<NodeJoinMemberWidget> {
     try {
       _currentPage++;
       List<ContractDelegatorItem> tempMemberList =
-          await _nodeApi.getContractDelegator(int.parse(widget.contractId), page: _currentPage);
+      await _nodeApi.getContractDelegator(int.parse(widget.contractId), page: _currentPage);
 
       if (tempMemberList.length > 0) {
         memberList.addAll(tempMemberList);
@@ -127,6 +127,7 @@ class _NodeJoinMemberState extends State<NodeJoinMemberWidget> {
               child: LoadDataContainer(
                   bloc: loadDataBloc,
                   enablePullDown: false,
+                  hasFootView: false,
                   //onLoadData: getJoinMemberData,
                   onLoadingMore: () {
                     getJoinMemberMoreData();
@@ -225,26 +226,17 @@ class _NodeJoinMemberState extends State<NodeJoinMemberWidget> {
         child: SizedBox(
           width: 91,
           height: 111,
-          child: Card(
-            /*decoration: BoxDecoration(
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
               borderRadius: BorderRadius.circular(16),
               boxShadow: [
                 BoxShadow(
-                  color: Colors.black12,
-                  offset: Offset(-0.01, -0.01),
-                  blurRadius: 0.5,
-                ),
-                BoxShadow(
-                  color: Colors.black12,
-                  offset: Offset(0.3, 0.3),
-                  blurRadius: 1,
-                ),
-                BoxShadow(
-                  color: Colors.white,
+                  color: Colors.grey[200],
+                  blurRadius: 40.0,
                 ),
               ],
-            ),*/
-            elevation: 2,
+            ),
             margin: const EdgeInsets.only(right: 12),
             child: Stack(
               children: <Widget>[
