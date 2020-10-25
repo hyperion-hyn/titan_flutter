be5271b11aabba3ef2b4ed95a73adea5d39e388f
naruto.j <naruto@163.com>
2020-10-25 14:56:26 +0800

    Update map3_node_detail_page.dart

--- lib/src/pages/atlas_map/map3/map3_node_detail_page.dart
+++ lib/src/pages/atlas_map/map3/map3_node_detail_page.dart
@@ -811,7 +811,7 @@ class _Map3NodeDetailState extends BaseState<Map3NodeDetailPage> {
               children: <Widget>[
                 iconMap3Widget(_map3infoEntity),
                 Expanded(
-                  flex: 1,
+                  flex: 2,
                   child: Padding(
                     padding: const EdgeInsets.only(left: 8, top: 2),
                     child: Column(
