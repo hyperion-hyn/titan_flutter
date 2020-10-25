3a8bc21c9ab40f9006a97be975293f5205456515
naruto.j <naruto@163.com>
2020-10-25 15:24:44 +0800

    Update log_util.dart

--- lib/src/utils/log_util.dart
+++ lib/src/utils/log_util.dart
@@ -54,7 +54,7 @@ class LogUtil {
       }
     } else if(error is RPCError){
       Fluttertoast.showToast(
-          msg: MemoryCache.contractErrorStr(error.message),
+          msg: MemoryCache.contractErrorStr("【错误码: ${error.errorCode}】：" + error.message),
           toastLength: Toast.LENGTH_LONG);
     } else {
       Fluttertoast.showToast(msg: error.toString());
