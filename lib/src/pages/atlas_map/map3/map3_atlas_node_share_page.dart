bdec69d4adb1674ef00242723ebcf8c232dbc17c
Lance <skyskyskyqz@gmail.com>
2020-10-27 12:00:28 +0800

    [map3]: hide share node

---
+++ lib/src/pages/atlas_map/map3/map3_atlas_node_share_page.dart
@@ -0,0 +1,172 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+import 'package:titan/src/basic/widget/base_state.dart';
+import 'package:titan/src/components/wallet/wallet_component.dart';
+import 'package:titan/src/pages/atlas_map/entity/map3_info_entity.dart';
+import 'package:titan/src/pages/node/model/contract_node_item.dart';
+import 'package:titan/src/basic/utils/hex_color.dart';
+import 'package:esys_flutter_share/esys_flutter_share.dart';
+import 'package:titan/generated/l10n.dart';
+import 'package:titan/src/pages/node/model/node_share_entity.dart';
+import 'package:titan/src/plugins/wallet/wallet.dart';
+import 'package:titan/src/utils/format_util.dart';
+
+import 'dart:typed_data';
+
+import 'package:titan/src/widget/widget_shot.dart';
+
+class Map3AtlasNodeSharePage extends StatefulWidget {
+  final Map3InfoEntity contractNodeItem;
+
+  Map3AtlasNodeSharePage(this.contractNodeItem);
+
+  @override
+  _Map3AtlasNodeSharePageState createState() => new _Map3AtlasNodeSharePageState();
+}
+
+// todo: test_1026
+class _Map3AtlasNodeSharePageState extends BaseState<Map3AtlasNodeSharePage> {
+  final ShotController _shotController = new ShotController();
+  List<String> imagesList = [];
+  var shareAppImage = "res/drawable/location_privacy.jpeg";
+  var shareData = "";
+
+  @override
+  void onCreated() {
+    var activityWallet = WalletInheritedModel.of(context).activatedWallet;
+    if (activityWallet != null) {
+      Wallet wallet = WalletInheritedModel.of(context).activatedWallet.wallet;
+      bool isFromOwn = wallet.getAtlasAccount().address == widget.contractNodeItem.address;
+      NodeShareEntity nodeShareEntity = NodeShareEntity(wallet.getEthAccount().address, "detail", isFromOwn);
+      String encodeStr = FormatUtil.encodeBase64(json.encode(nodeShareEntity));
+      shareData = "${widget.contractNodeItem.shareUrl}&key=$encodeStr";
+    } else {
+      shareData = "${widget.contractNodeItem.shareUrl}";
+    }
+    super.onCreated();
+
+    Future.delayed(Duration(milliseconds: 500)).then((_) {
+      _shareQr(context);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          S.of(context).share_contract,
+          style: TextStyle(color: Colors.white),
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.share),
+            color: Colors.white,
+            tooltip: S.of(context).share,
+            onPressed: () {
+              _shareQr(context);
+            },
+          ),
+        ],
+        centerTitle: true,
+        iconTheme: IconThemeData(color: Colors.white),
+      ),
+      body: _body(context),
+    );
+  }
+
+  Widget _body(BuildContext context) {
+//    var userInfo = AccountInheritedModel.of(context, aspect: AccountAspect.userInfo).userInfo;
+
+    print("[map3]  shareData:$shareData");
+
+    var wallet = WalletInheritedModel.of(context).activatedWallet;
+    return WidgetShot(
+      controller: _shotController,
+      child: Container(
+        decoration: BoxDecoration(
+          gradient: new LinearGradient(begin: Alignment.centerLeft, end: Alignment.centerRight, colors: [
+            HexColor("#1095b0"),
+            HexColor("#137291"),
+          ]),
+        ),
+        child: Column(
+          children: <Widget>[
+            Stack(
+              alignment: Alignment.center,
+              children: <Widget>[
+                Container(
+                  height: 264,
+                  decoration: BoxDecoration(
+                    color: HexColor("#FFFFFEF8"),
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(50),
+                      bottomRight: Radius.circular(50),
+                    ), // 也可控件一边圆角大小
+                  ),
+                ),
+                Positioned(
+                  top: 60,
+                  child: Image.asset(
+                    "res/drawable/ic_map3_node_item_2.png",
+                    width: 168,
+                    height: 168,
+                  ),
+                ),
+                Positioned(
+                  left: 16,
+                  top: 25,
+                  child: Image.asset(
+                    "res/drawable/ic_logo.png",
+                    width: 40,
+                    height: 40,
+                    //color: Color(0xFFD9AC43),
+                    color: Theme.of(context).primaryColor,
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 24, right: 24.0, top: 40),
+              child: Text(
+                '「${wallet?.wallet?.keystore?.name ?? ''}」在泰坦地图发现了海波利安节点, 邀请你前往围观！',
+                /*
+                S.of(context).contract_share_content(
+                    wallet?.wallet?.keystore?.name ?? '',
+                    S.of(context).app_name,
+                    widget?.contractNodeItem?.contract?.nodeName??"",
+                    FormatUtil.formatPercent(widget.contractNodeItem?.contract?.annualizedYield??0),
+                    widget.contractNodeItem?.contract?.duration?.toString()??""),
+                */
+                style: TextStyle(color: Colors.white, fontSize: 16),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 40.0, bottom: 32),
+              child: Container(
+                color: Colors.white,
+                child: QrImage(
+                  data: shareData,
+                  size: 131,
+                ),
+              ),
+            ),
+            Text(
+              S.of(context).scan_qr_code_detail,
+              style: TextStyle(color: Colors.white, fontSize: 15),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _shareQr(BuildContext context) async {
+    Uint8List imageByte = await _shotController.makeImageUint8List();
+    await Share.file(S.of(context).nav_share_app, 'app.png', imageByte, 'image/png');
+  }
+}
