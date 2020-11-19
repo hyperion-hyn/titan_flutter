0a90acb52da11f26ea4f25726e9b33f54f843b2d
Lance <skyskyskyqz@gmail.com>
2020-11-19 16:20:28 +0800

    [atlas]: update hyn burn banner

--- lib/src/pages/atlas_map/widget/hyn_burn_banner.dart
+++ lib/src/pages/atlas_map/widget/hyn_burn_banner.dart
@@ -1,21 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:titan/generated/l10n.dart';
-import 'package:titan/src/basic/utils/hex_color.dart';
-import 'package:titan/src/components/atlas/atlas_component.dart';
-import 'package:titan/src/config/application.dart';
 import 'package:titan/src/pages/atlas_map/api/atlas_api.dart';
 import 'package:titan/src/pages/atlas_map/atlas/burn_history_page.dart';
 import 'package:titan/src/pages/atlas_map/entity/burn_history.dart';
-import 'package:titan/src/pages/atlas_map/entity/enum_atlas_type.dart';
-import 'package:titan/src/pages/atlas_map/entity/map3_info_entity.dart';
-import 'package:titan/src/pages/atlas_map/map3/map3_node_public_widget.dart';
-import 'package:titan/src/pages/node/model/map3_node_util.dart';
-import 'package:titan/src/routes/fluro_convert_utils.dart';
-import 'package:titan/src/routes/routes.dart';
-import 'package:titan/src/style/titan_sytle.dart';
-import 'package:titan/src/utils/image_util.dart';
-import 'package:titan/src/widget/wallet_widget.dart';
 
 class HynBurnBanner extends StatefulWidget {
   HynBurnBanner();
@@ -56,56 +43,56 @@ class _HynBurnBannerState extends State<HynBurnBanner> {
 
   @override
   Widget build(BuildContext context) {
+    var round = _burnMsg?.latestBurnHistory?.id;
+    var msg = round != null ? '第$round轮 HYN燃烧完成' : '';
     return InkWell(
       onTap: () {
         Navigator.of(context).push(MaterialPageRoute(
           builder: (BuildContext context) => BurnHistoryPage(),
         ));
       },
-      child: _burnMsg?.latestBurnHistory != null
-          ? Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Container(
+      child: Padding(
+        padding: EdgeInsets.symmetric(vertical: 16.0),
+        child: Container(
+          height: 50,
+          child: Stack(
+            children: [
+              Image.asset(
+                'res/drawable/bg_banner_hyn_burn.png',
                 height: 50,
-                child: Stack(
+              ),
+              Container(
+                height: 60,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    Image.asset(
-                      'res/drawable/bg_banner_hyn_burn.png',
-                      height: 50,
+                    SizedBox(
+                      width: 120,
                     ),
-                    Container(
-                      height: 60,
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          SizedBox(
-                            width: 120,
-                          ),
-                          Expanded(
-                            child: Text(
-                              '第${_burnMsg.latestBurnHistory?.id}轮 HYN燃烧完成',
-                              style: TextStyle(
-                                fontSize: 13,
-                                color: Theme.of(context).primaryColor,
-                              ),
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(16.0),
-                            child: Image.asset(
-                              'res/drawable/ic_rounded_arrow.png',
-                              width: 20,
-                              height: 20,
-                            ),
-                          ),
-                        ],
+                    Expanded(
+                      child: Text(
+                        msg,
+                        style: TextStyle(
+                          fontSize: 13,
+                          color: Theme.of(context).primaryColor,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Image.asset(
+                        'res/drawable/ic_rounded_arrow.png',
+                        width: 20,
+                        height: 20,
                       ),
                     ),
                   ],
                 ),
               ),
-            )
-          : Container(),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
