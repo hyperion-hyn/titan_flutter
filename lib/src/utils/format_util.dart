2a5057f232981a63ab54147f6e51836fc0a0d75a
naruto.j <naruto@163.com>
2020-11-28 09:35:12 +0800

    Update format_util.dart

--- lib/src/utils/format_util.dart
+++ lib/src/utils/format_util.dart
@@ -354,7 +354,14 @@ class FormatUtil {
       return entityParam;
     }
     if (entityParam is String) {
-      return ConvertTokenUnit.weiToEther(weiBigInt: BigInt.parse(entityParam)).toString();
+      var arr = entityParam.split('.');
+      bool isContainerPoint = (arr?.length ?? 0) > 1;
+      String pendingValue = entityParam;
+      if (isContainerPoint) {
+        pendingValue = arr?.first ?? '0';
+      }
+      var weiBigInt = (BigInt.tryParse(pendingValue) ?? BigInt.from(int.tryParse(pendingValue) ?? 0));
+      return ConvertTokenUnit.weiToEther(weiBigInt: weiBigInt).toString();
     } else if (entityParam is int) {
       return ConvertTokenUnit.weiToEther(weiInt: entityParam).toString();
     } else {
