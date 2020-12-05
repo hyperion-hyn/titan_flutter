926375fc1e9ded5d9268df3ad9e61a71f0f91f5a
naruto.j <naruto@163.com>
2020-12-05 17:34:05 +0800

    Update exchange_page.dart

--- lib/src/pages/market/exchange/exchange_page.dart
+++ lib/src/pages/market/exchange/exchange_page.dart
@@ -46,8 +46,7 @@ class ExchangePage extends StatefulWidget {
 }
 
 
-class _ExchangePageState extends BaseState<ExchangePage>
-    with AutomaticKeepAliveClientMixin {
+class _ExchangePageState extends BaseState<ExchangePage>  {
 
   var _selectedCoin = 'USDT';
   var _exchangeType = ExchangeType.BUY;
