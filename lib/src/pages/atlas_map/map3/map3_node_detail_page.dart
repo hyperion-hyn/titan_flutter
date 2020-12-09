a70b52df5e31c27b92210a6ac92ba26ab168f881
naruto.j <naruto@163.com>
2020-12-09 14:48:06 +0800

    【Fix】 fix scrollViewAble issu

--- lib/src/pages/atlas_map/map3/map3_node_detail_page.dart
+++ lib/src/pages/atlas_map/map3/map3_node_detail_page.dart
@@ -2321,7 +2321,7 @@ class _Map3NodeDetailState extends BaseState<Map3NodeDetailPage> with TickerProv
         */
         ListView.builder(
           shrinkWrap: true,
-          physics: NeverScrollableScrollPhysics(),
+          physics: BouncingScrollPhysics(),
           scrollDirection: Axis.vertical,
           itemBuilder: (context, index) {
             return delegateRecordItemWidget(
@@ -2371,7 +2371,7 @@ class _Map3NodeDetailState extends BaseState<Map3NodeDetailPage> with TickerProv
         ),
         ListView.builder(
           shrinkWrap: true,
-          physics: NeverScrollableScrollPhysics(),
+          physics: BouncingScrollPhysics(),
           scrollDirection: Axis.vertical,
           itemBuilder: (context, index) {
             var item = _userList[index];
