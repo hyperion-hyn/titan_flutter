55247226d175ae6efccf613b9e5bc2697c51cdcd
naruto.j <naruto@163.com>
2020-12-11 15:04:42 +0800

    【add] add config todo

--- lib/src/pages/red_pocket/rp_level_upgrade_page.dart
+++ lib/src/pages/red_pocket/rp_level_upgrade_page.dart
@@ -313,7 +313,8 @@ class _RpLevelUpgradeState extends BaseState<RpLevelUpgradePage> {
                       right: 16,
                     ),
                     child: Text(
-                      '提示：因你还没有推荐人，系统将为你随机设定一个量级4以上的账户地址为推荐人',
+                      // todo: 配置量级4
+                      '提示：因你还没有推荐人，系统将为你随机设定一个量级${levelValueToLevelName(4)}以上的账户地址为推荐人',
                       style: TextStyle(
                         color: HexColor('#C3A16D'),
                         fontSize: 12,
