daca4d3ed62c4da3b6593dd35c2ab8ac69983421
naruto.j <naruto@163.com>
2020-12-14 12:55:52 +0800

    [update] update rp component

--- lib/src/components/rp/bloc/redpocket_bloc.dart
+++ lib/src/components/rp/bloc/redpocket_bloc.dart
@@ -1,7 +1,5 @@
 import 'dart:async';
 import 'package:bloc/bloc.dart';
-import 'package:titan/src/components/wallet/wallet_component.dart';
-import 'package:titan/src/config/consts.dart';
 import 'package:titan/src/pages/red_pocket/api/rp_api.dart';
 import './bloc.dart';
 
@@ -17,8 +15,11 @@ class RedPocketBloc extends Bloc<RedPocketEvent, RedPocketState> {
   ) async* {
 
     if (event is UpdateMyLevelInfoEntityEvent) {
+
       try {
-        var _address = WalletInheritedModel.of(Keys.rootKey.currentContext)?.activatedWallet?.wallet?.getAtlasAccount()?.address ?? '';
+        var _address = event.address;
+        //print("[RedPocketBloc] UpdateMyLevelInfoEntityEvent, _address:$_address");
+
         var _myLevelInfo = await _rpApi.getRPMyLevelInfo(_address);
 
         yield UpdateMyLevelInfoEntityState(_myLevelInfo);
