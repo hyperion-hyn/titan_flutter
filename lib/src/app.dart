c197cea231280f12edc92c72c513b7dd1d21e92e
naruto.j <naruto@163.com>
2020-12-22 19:42:10 +0800

    Update app.dart

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,4 +1,3 @@
-import 'package:fluro/fluro.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -6,14 +5,13 @@ import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:oktoast/oktoast.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart';
 import 'package:titan/generated/l10n.dart';
+import 'package:titan/src/components/account/account_component.dart';
 import 'package:titan/src/components/atlas/atlas_component.dart';
 import 'package:titan/src/components/auth/auth_component.dart';
 import 'package:titan/src/components/exchange/exchange_component.dart';
 import 'package:titan/src/components/rp/redpocket_component.dart';
 import 'package:titan/src/config/consts.dart';
 import 'package:titan/src/components/style/theme.dart';
-
-import 'components/auth/bloc/auth_bloc.dart';
 import 'components/root_page_control_component/bloc/bloc.dart';
 import 'components/setting/setting_component.dart';
 import 'components/socket/socket_component.dart';
@@ -46,47 +44,49 @@ class _AppState extends State<App> {
               child: SocketComponent(
                 child: AtlasComponent(
                   child: RedPocketComponent(
-                    child: MultiBlocProvider(
-                      providers: [
-                        BlocProvider<UpdateBloc>(create: (context) => UpdateBloc(context: context)),
-                        BlocProvider<RootPageControlBloc>(create: (context) => RootPageControlBloc()),
-                      ],
-                      child: Builder(
-                        builder: (context) {
-                          return RefreshConfiguration(
-                            //pull to refresh config
-                            dragSpeedRatio: 0.91,
-                            headerTriggerDistance: 80,
-                            footerTriggerDistance: 80,
-                            maxOverScrollExtent: 100,
-                            maxUnderScrollExtent: 0,
-                            headerBuilder: () => WaterDropMaterialHeader(),
-                            footerBuilder: () => ClassicFooter(),
-                            autoLoad: true,
-                            enableLoadingWhenFailed: false,
-                            hideFooterWhenNotFull: true,
-                            enableBallisticLoad: true,
-                            child: MaterialApp(
-                              key: Keys.materialAppKey,
-                              debugShowCheckedModeBanner: false,
-                              locale: SettingInheritedModel.of(context, aspect: SettingAspect.language)
-                                  .languageModel
-                                  ?.locale,
-                              title: 'titan',
-                              theme: appTheme,
-                              localizationsDelegates: [
-                                S.delegate,
-                                GlobalMaterialLocalizations.delegate,
-                                GlobalWidgetsLocalizations.delegate,
-                                GlobalCupertinoLocalizations.delegate,
-                                RefreshLocalizations.delegate,
-                              ],
-                              supportedLocales: S.delegate.supportedLocales,
-                              navigatorObservers: [Application.routeObserver],
-                              onGenerateRoute: Application.router.generator,
-                            ),
-                          );
-                        },
+                    child: AccountComponent(
+                      child: MultiBlocProvider(
+                        providers: [
+                          BlocProvider<UpdateBloc>(create: (context) => UpdateBloc(context: context)),
+                          BlocProvider<RootPageControlBloc>(create: (context) => RootPageControlBloc()),
+                        ],
+                        child: Builder(
+                          builder: (context) {
+                            return RefreshConfiguration(
+                              //pull to refresh config
+                              dragSpeedRatio: 0.91,
+                              headerTriggerDistance: 80,
+                              footerTriggerDistance: 80,
+                              maxOverScrollExtent: 100,
+                              maxUnderScrollExtent: 0,
+                              headerBuilder: () => WaterDropMaterialHeader(),
+                              footerBuilder: () => ClassicFooter(),
+                              autoLoad: true,
+                              enableLoadingWhenFailed: false,
+                              hideFooterWhenNotFull: true,
+                              enableBallisticLoad: true,
+                              child: MaterialApp(
+                                key: Keys.materialAppKey,
+                                debugShowCheckedModeBanner: false,
+                                locale: SettingInheritedModel.of(context, aspect: SettingAspect.language)
+                                    .languageModel
+                                    ?.locale,
+                                title: 'titan',
+                                theme: appTheme,
+                                localizationsDelegates: [
+                                  S.delegate,
+                                  GlobalMaterialLocalizations.delegate,
+                                  GlobalWidgetsLocalizations.delegate,
+                                  GlobalCupertinoLocalizations.delegate,
+                                  RefreshLocalizations.delegate,
+                                ],
+                                supportedLocales: S.delegate.supportedLocales,
+                                navigatorObservers: [Application.routeObserver],
+                                onGenerateRoute: Application.router.generator,
+                              ),
+                            );
+                          },
+                        ),
                       ),
                     ),
                   ),
