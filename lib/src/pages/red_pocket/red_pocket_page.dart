b2991a13746b0951bd8b8e3987bf1e88bd409dd6
naruto.j <naruto@163.com>
2021-01-13 10:24:05 +0800

    [Add] add rp_floating widget

--- lib/src/pages/red_pocket/red_pocket_page.dart
+++ lib/src/pages/red_pocket/red_pocket_page.dart
@@ -24,6 +24,7 @@ import 'package:titan/src/pages/red_pocket/rp_share_open_page.dart';
 import 'package:titan/src/pages/red_pocket/rp_transmit_page.dart';
 import 'package:titan/src/pages/red_pocket/rp_share_type_page.dart';
 import 'package:titan/src/pages/red_pocket/widget/rp_airdrop_widget.dart';
+import 'package:titan/src/pages/red_pocket/widget/rp_floating_widget.dart';
 import 'package:titan/src/pages/red_pocket/widget/rp_level_widget.dart';
 import 'package:titan/src/pages/red_pocket/widget/rp_statistics_widget.dart';
 import 'package:titan/src/pages/wallet/wallet_manager/wallet_manager_page.dart';
@@ -137,62 +138,11 @@ class _RedPocketPageState extends BaseState<RedPocketPage> with RouteAware {
                   ],
                 )),
           ),
-          /*if (_latestRoundInfo != null) Positioned(
-            right: 0,
-            top: MediaQuery.of(context).size.height * 0.5,
-            child: _floatingWidget(),
-          ),
-          */
-          if (_latestRoundInfo != null) AppFloatBox(),
+
+          if (_latestRoundInfo != null) RpFloatingWidget(),
         ],
       ),
-      //floatingActionButton: _floatingWidget(),
-    );
-  }
 
-  Widget _floatingWidget() {
-    return SizedBox(
-      width: 80,
-      height: 80,
-      child: IconButton(
-        onPressed: _navToShareRp,
-        icon: Stack(
-          alignment: Alignment.topCenter,
-          children: [
-            Image.asset(
-              'res/drawable/rp_share_floating.png',
-              // width: 80,
-              // height: 69,
-              fit: BoxFit.cover,
-              // color: HexColor('#FF1F81FF'),
-            ),
-            Positioned(
-              top: 30,
-              child: Column(
-                //crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text(
-                    '攒好友',
-                    style: TextStyle(
-                      color: HexColor('#FFFFFF'),
-                      fontWeight: FontWeight.w500,
-                      fontSize: 6,
-                    ),
-                  ),
-                  Text(
-                    '发红包',
-                    style: TextStyle(
-                      color: HexColor('#FFFFFF'),
-                      fontWeight: FontWeight.w500,
-                      fontSize: 8,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
     );
   }
 
@@ -1002,145 +952,6 @@ class _RedPocketPageState extends BaseState<RedPocketPage> with RouteAware {
       }
     } catch (e) {
       _loadDataBloc.add(RefreshFailEvent());
-    }
-  }
-}
-
-class AppFloatBox extends StatefulWidget {
-  @override
-  _AppFloatBoxState createState() => _AppFloatBoxState();
-}
-
-class _AppFloatBoxState extends BaseState<AppFloatBox> {
-  Offset offset = Offset(10, 0);
-
-  Offset _calOffset(Size size, Offset offset, Offset nextOffset) {
-    double dx = 0;
-    //水平方向偏移量不能小于0不能大于屏幕最大宽度
-    if (offset.dx + nextOffset.dx <= 0) {
-      dx = 10;
-    } else if (offset.dx + nextOffset.dx >= (size.width - 80)) {
-      dx = size.width - 80;
-    } else {
-      dx = offset.dx + nextOffset.dx;
-    }
-
-    double dy = 0;
-    //垂直方向偏移量不能小于0不能大于屏幕最大高度
-    if (offset.dy + nextOffset.dy <= 0) {
-      dy = 0;
-    } else if (offset.dy + nextOffset.dy >= (size.height - 150)) {
-      dy = size.height - 150;
-      //print("[$runtimeType] 1, dy:$dy");
-    } else {
-      dy = offset.dy + nextOffset.dy;
-      //print("[$runtimeType] 2, dy:$dy");
-    }
-
-    print("[$runtimeType] size:${size.toString()}, dy:$dy");
-
-    return Offset(
-      dx,
-      dy,
-    );
-  }
-
-  @override
-  void initState() {
-    super.initState();
-
-
-  }
-
-  @override
-  void onCreated() {
-    super.onCreated();
-
-    var size = MediaQuery.of(context).size;
-    offset = Offset(size.width - 80, size.height * 0.5);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Positioned(
-      left: offset.dx,
-      top: offset.dy,
-      child: GestureDetector(
-        onPanUpdate: (detail) {
-          setState(() {
-            offset = _calOffset(MediaQuery.of(context).size, offset, detail.delta);
-          });
-        },
-        onPanEnd: (detail) {},
-        child: _floatingWidget(),
-      ),
-    );
-  }
-
-  Widget _floatingWidget() {
-    return SizedBox(
-      width: 80,
-      height: 80,
-      child: IconButton(
-        onPressed: _navToShareRp,
-        icon: Stack(
-          alignment: Alignment.topCenter,
-          children: [
-            Image.asset(
-              'res/drawable/rp_share_floating.png',
-              // width: 80,
-              // height: 69,
-              fit: BoxFit.cover,
-              // color: HexColor('#FF1F81FF'),
-            ),
-            Positioned(
-              top: 30,
-              child: Column(
-                //crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text(
-                    '攒好友',
-                    style: TextStyle(
-                      color: HexColor('#FFFFFF'),
-                      fontWeight: FontWeight.w500,
-                      fontSize: 6,
-                    ),
-                  ),
-                  Text(
-                    '发红包',
-                    style: TextStyle(
-                      color: HexColor('#FFFFFF'),
-                      fontWeight: FontWeight.w500,
-                      fontSize: 8,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  _navToShareRp() {
-    var activeWallet = WalletInheritedModel.of(context)?.activatedWallet;
-
-    // var _address  = activeWallet.wallet.getAtlasAccount().address;
-    // var _walletName = activeWallet.wallet.keystore.name;
-    // showShareRpOpenDialog(context:context, id:'BVABHH', address:_address, walletName: _walletName,);
-    //
-    // return;
-
-    if (activeWallet != null) {
-      Navigator.push(
-        context,
-        MaterialPageRoute(
-          builder: (context) => RpShareTypePage(),
-        ),
-      );
-    } else {
-      Fluttertoast.showToast(msg: S.of(context).create_or_import_wallet_first);
     }
   }
 }
---
+++ lib/src/pages/red_pocket/widget/rp_floating_widget.dart
@@ -0,0 +1,146 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:titan/generated/l10n.dart';
+import 'package:titan/src/basic/utils/hex_color.dart';
+import 'package:titan/src/basic/widget/base_state.dart';
+import 'package:titan/src/components/wallet/wallet_component.dart';
+import 'package:titan/src/pages/red_pocket/rp_share_type_page.dart';
+
+class RpFloatingWidget extends StatefulWidget {
+  @override
+  _RpFloatingWidgetState createState() => _RpFloatingWidgetState();
+}
+
+class _RpFloatingWidgetState extends BaseState<RpFloatingWidget> {
+  Offset offset = Offset(10, 0);
+
+  Offset _calOffset(Size size, Offset offset, Offset nextOffset) {
+    double dx = 0;
+    //水平方向偏移量不能小于0不能大于屏幕最大宽度
+    if (offset.dx + nextOffset.dx <= 0) {
+      dx = 10;
+    } else if (offset.dx + nextOffset.dx >= (size.width - 80)) {
+      dx = size.width - 80;
+    } else {
+      dx = offset.dx + nextOffset.dx;
+    }
+
+    double dy = 0;
+    //垂直方向偏移量不能小于0不能大于屏幕最大高度
+    if (offset.dy + nextOffset.dy <= 0) {
+      dy = 0;
+    } else if (offset.dy + nextOffset.dy >= (size.height - 150)) {
+      dy = size.height - 150;
+      //print("[$runtimeType] 1, dy:$dy");
+    } else {
+      dy = offset.dy + nextOffset.dy;
+      //print("[$runtimeType] 2, dy:$dy");
+    }
+
+    print("[$runtimeType] size:${size.toString()}, dy:$dy");
+
+    return Offset(
+      dx,
+      dy,
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+
+  }
+
+  @override
+  void onCreated() {
+    super.onCreated();
+
+    var size = MediaQuery.of(context).size;
+    offset = Offset(size.width - 80, size.height * 0.5);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      left: offset.dx,
+      top: offset.dy,
+      child: GestureDetector(
+        onPanUpdate: (detail) {
+          setState(() {
+            offset = _calOffset(MediaQuery.of(context).size, offset, detail.delta);
+          });
+        },
+        onPanEnd: (detail) {},
+        child: _floatingWidget(),
+      ),
+    );
+  }
+
+  Widget _floatingWidget() {
+    return SizedBox(
+      width: 80,
+      height: 80,
+      child: IconButton(
+        onPressed: _navToShareRp,
+        icon: Stack(
+          alignment: Alignment.topCenter,
+          children: [
+            Image.asset(
+              'res/drawable/rp_share_floating.png',
+              // width: 80,
+              // height: 69,
+              fit: BoxFit.cover,
+              // color: HexColor('#FF1F81FF'),
+            ),
+            Positioned(
+              top: 30,
+              child: Column(
+                //crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    '攒好友',
+                    style: TextStyle(
+                      color: HexColor('#FFFFFF'),
+                      fontWeight: FontWeight.w500,
+                      fontSize: 6,
+                    ),
+                  ),
+                  Text(
+                    '发红包',
+                    style: TextStyle(
+                      color: HexColor('#FFFFFF'),
+                      fontWeight: FontWeight.w500,
+                      fontSize: 8,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  _navToShareRp() {
+    var activeWallet = WalletInheritedModel.of(context)?.activatedWallet;
+
+    // var _address  = activeWallet.wallet.getAtlasAccount().address;
+    // var _walletName = activeWallet.wallet.keystore.name;
+    // showShareRpOpenDialog(context:context, id:'BVABHH', address:_address, walletName: _walletName,);
+    //
+    // return;
+
+    if (activeWallet != null) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => RpShareTypePage(),
+        ),
+      );
+    } else {
+      Fluttertoast.showToast(msg: S.of(context).create_or_import_wallet_first);
+    }
+  }
+}
