6e80405a2011cc48af7cbb497bd202f6023f4db7
Lance <skyskyskyqz@gmail.com>
2021-01-20 15:06:20 +0800

    [check-update]: remove old update code

--- lib/src/components/updater/bloc/update_bloc.dart
+++ lib/src/components/updater/bloc/update_bloc.dart
@@ -11,46 +11,6 @@ import 'package:titan/src/global.dart';
 import 'package:titan/src/pages/atlas_map/api/atlas_api.dart';
 import 'bloc.dart';
 
-/*
-class UpdateBlocOld extends Bloc<UpdateEvent, UpdateState> {
-  BuildContext context;
-
-  UpdateBloc({this.context});
-
-  @override
-  UpdateState get initialState => InitialAppState();
-
-  @override
-  Stream<UpdateState> mapEventToState(UpdateEvent event) async* {
-    if (event is CheckUpdate) {
-      yield UpdateCheckState(isChecking: true);
-      try {
-        var injector = Injector.of(context);
-        var channel = "";
-        if (env.channel == BuildChannel.OFFICIAL) {
-          channel = "official";
-        } else if (env.channel == BuildChannel.STORE) {
-          channel = "store";
-        }
-
-        var platform = "";
-        if (Platform.isAndroid) {
-          platform = "android";
-        } else if (Platform.isIOS) {
-          platform = "ios";
-        }
-        var versionModel = await injector.repository.checkNewVersion(channel, event.lang, platform);
-        yield UpdateCheckState(isChecking: false, updateEntity: versionModel, isManual: event.isManual);
-      } catch (err) {
-        logger.e(err);
-        yield UpdateCheckState(isError: true, isChecking: false, isManual: event.isManual);
-      }
-    }
-  }
-}
-*/
-
-
 class UpdateBloc extends Bloc<UpdateEvent, UpdateState> {
   BuildContext context;
 
@@ -64,33 +24,22 @@ class UpdateBloc extends Bloc<UpdateEvent, UpdateState> {
     if (event is CheckUpdate) {
       yield UpdateCheckState(isChecking: true);
       try {
-        //var injector = Injector.of(context);
-        // var channel = "";
-        // if (env.channel == BuildChannel.OFFICIAL) {
-        //   channel = "official";
-        // } else if (env.channel == BuildChannel.STORE) {
-        //   channel = "store";
-        // }
-        //
-        // var platform = "";
-        // if (Platform.isAndroid) {
-        //   platform = "android";
-        // } else if (Platform.isIOS) {
-        //   platform = "ios";
-        // }
-        // var versionModel = await injector.repository.checkNewVersion(channel, event.lang, platform);
         var appUpdateInfo;
         try {
           appUpdateInfo = await AtlasApi.checkUpdate();
         } catch (e) {}
         yield UpdateCheckState(
-            isChecking: false,
-            appUpdateInfo: appUpdateInfo,
-            isManual: event.isManual);
+          isChecking: false,
+          appUpdateInfo: appUpdateInfo,
+          isManual: event.isManual,
+        );
       } catch (err) {
         logger.e(err);
         yield UpdateCheckState(
-            isError: true, isChecking: false, isManual: event.isManual);
+          isError: true,
+          isChecking: false,
+          isManual: event.isManual,
+        );
       }
     }
   }
--- lib/src/components/updater/bloc/update_state.dart
+++ lib/src/components/updater/bloc/update_state.dart
@@ -14,50 +14,22 @@ class InitialAppState extends UpdateState {}
 class UpdateCheckState extends UpdateState {
   final bool isManual;
 
-
-  UpdateCheckState({AppUpdateInfo appUpdateInfo, bool isError, bool isChecking, this.isManual = false}) {
+  UpdateCheckState({
+    AppUpdateInfo appUpdateInfo,
+    bool isError,
+    bool isChecking,
+    this.isManual = false,
+  }) {
     appData.appUpdateInfo = appUpdateInfo;
     appData.isError = isError ?? appData.isError;
     appData.isChecking = isChecking ?? appData.isChecking;
   }
-
-
-  /*
-  UpdateCheckStateOld({UpdateEntity updateEntity, bool isError, bool isChecking, this.isManual = false}) {
-    appData.updateEntity = updateEntity;
-    appData.isError = isError ?? appData.isError;
-    appData.isChecking = isChecking ?? appData.isChecking;
-  }
-  */
 
   @override
   String toString() {
     return 'UpdateState(isChecking: ${appData.isChecking}, isError: ${appData.isError}, versionModel: ${appData.appUpdateInfo})';
   }
 }
-
-/*
-class AppData {
-  //-------------
-  //  update
-  //-------------
-  bool isChecking = false;
-  UpdateEntity updateEntity;
-  bool isError = false;
-
-  AppData({
-    this.updateEntity,
-    this.isError,
-    this.isChecking,
-  });
-
-  @override
-  String toString() {
-    return 'AppData(isChecking: $isChecking, isError: $isError,versionModel: $updateEntity)';
-  }
-}
-*/
-
 
 class AppData {
   //-------------
