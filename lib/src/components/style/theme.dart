56d6e4fae49e17e0db7eb95b5f1274d9446a8350
naruto.j <naruto@163.com>
2021-01-29 10:37:56 +0800

    【update】 update  theme

--- lib/src/components/style/theme.dart
+++ lib/src/components/style/theme.dart
@@ -2,6 +2,12 @@ import 'package:flutter/material.dart';
 
 ThemeData appTheme = appThemeDeepYellow;
 
+ThemeData appThemeDeepYellow = ThemeData.light().copyWith(
+  primaryColor: Color(0xffedc313),
+  backgroundColor: Color(0xfff9f9f9),
+  appBarTheme: AppBarTheme(brightness: Brightness.light),
+);
+
 ThemeData appThemeDefault = ThemeData.light().copyWith(
     primaryColor: Color(0xff1F81FF),
     backgroundColor: Color(0xfff9f9f9),
@@ -16,9 +22,3 @@ ThemeData appThemeDeepRed = ThemeData.light().copyWith(
     primaryColor: Color(0xffcc5858),
     backgroundColor: Color(0xfff9f9f9),
     appBarTheme: AppBarTheme(brightness: Brightness.light));
-
-ThemeData appThemeDeepYellow = ThemeData.light().copyWith(
-    primaryColor: Color(0xffedc313),
-    backgroundColor: Color(0xfff9f9f9),
-    appBarTheme: AppBarTheme(brightness: Brightness.light),
-);
